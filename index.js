1852a4874020b8eaa7ac002799822ea3fabdba27
Jamal Nabi <Jamalnabi07@gmail.com>
2020-03-12 13:05:34 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const http = require('http');
+const bodyParser = require('body-parser');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+//DB setup
+const uri = process.env.MONGO_URL;
+mongoose.connect(uri,{ useNewUrlParser : true, useCreateIndex : true });
+
+//App setup
+app.use(morgan('combined'));//Used for logging and it is middleware
+app.use(bodyParser.json({ type : '*/*' }));//It is middleware
+router(app);
+
+//server setup
+const port = process.env.PORT || 71111;
+const server = http.createServer(app);
+server.listen(port);
+
+console.log(`Server is listening on port :`, port );
